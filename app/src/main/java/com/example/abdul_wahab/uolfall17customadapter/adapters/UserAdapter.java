1fe440bcad7cf379ab1de22bdd090c6a390be95e
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-09-29 10:55:30 +0500

    View Recycling and ViewHolder Pattern

--- app/src/main/java/com/example/abdul_wahab/uolfall17customadapter/adapters/UserAdapter.java
+++ app/src/main/java/com/example/abdul_wahab/uolfall17customadapter/adapters/UserAdapter.java
@@ -3,6 +3,7 @@ package com.example.abdul_wahab.uolfall17customadapter.adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,13 @@ import java.util.List;
 
 public class UserAdapter extends ArrayAdapter<User> {
 
+    class ViewHolder {
+        TextView tvUsername;
+        TextView tvRollNumber;
+    }
+
+
+    private static final String TAG = "MTAG";
 
     public UserAdapter(@NonNull Context context, @NonNull List<User> objects) {
         super(context, 0, objects);
@@ -29,14 +37,27 @@ public class UserAdapter extends ArrayAdapter<User> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.user_item_list, parent,false);
+        Log.d(TAG, "convertview : " + convertView);
+        View view = convertView;
+        ViewHolder vh;
         User u = getItem(position);
-        TextView tvUsername = (TextView) view.findViewById(R.id.tvUsername);
-        TextView tvRollNumber = (TextView) view.findViewById(R.id.tvRollNumber);
 
-        tvUsername.setText(u.getUsername());
-        tvRollNumber.setText(u.getRollnumber());
+        if (view == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.user_item_list, parent, false);
+
+            TextView tvUsername = (TextView) view.findViewById(R.id.tvUsername);
+            TextView tvRollNumber = (TextView) view.findViewById(R.id.tvRollNumber);
+
+            vh = new ViewHolder();
+            vh.tvUsername = tvUsername;
+            vh.tvRollNumber = tvRollNumber;
+            view.setTag(vh);
+        }
+
+        vh = (ViewHolder) view.getTag();
+
+        vh.tvUsername.setText(u.getUsername());
+        vh.tvRollNumber.setText(u.getRollnumber());
 
 
         return view;
